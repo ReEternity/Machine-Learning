7d22938e75ce20dd08b6b08b9ac7182b3baf863a
ReEternity <kamatworks@gmail.com>
2023-09-24 17:41:05 -0400

    fix

--- gpaPredictor.py
+++ gpaPredictor.py
@@ -30,36 +30,48 @@ def main():
 
     elif choice == "Single GPA Prediction":
         # Add a title and some text to the app:
-        st.title("Single Prediction")
-        st.write("Enter the necessary input and see live predictions.")
+        st.title("Batch Prediction")
+        st.write("Upload a CSV file and see live predictions.")
 
-        # Add your input fields here
-        # For example:
-        input_text = st.text_input("Enter text for prediction")
+        # Add a file uploader to upload a CSV file
+        uploaded_file = st.file_uploader("Upload CSV file", type=["csv"])
 
-        # Add a button to trigger the prediction
-        if st.button("Predict"):
-            # Call your model and perform the prediction here
-            # For example:
-            prediction = _singlePredict(input_text)
+        # If a file is uploaded, process and display predictions
+        if uploaded_file is not None:
+            try:
+              df = pd.read_csv(uploaded_file)
+            except Exception as e:
+                st.error("Error: Invalid CSV file. Please upload a valid CSV file.")
+            # Display the uploaded data
+            st.subheader("Input Data")
+            st.dataframe(df, use_container_width=True)
 
-            # Display the prediction result
-            st.subheader(f"Prediction: {prediction}")
+            # Perform predictions on the uploaded data
+            predictions = _batchPredict(df)
+
+            # Display the prediction results
+            st.subheader("Prediction Results")
+            st.dataframe(predictions, use_container_width=True)
 
 # Define your model prediction function here
 # For example:
 
 # We are going to use st.cache to improve performance for predictions.
 @st.cache_data
-def _singlePredict(input_text):
-    # Format the input_text so that you can pass it to the model
+def _batchPredict(df):
+    # Format the dataframe so that you can pass it to the model
     # For example:
-    # Call your model to make predictions on the input_text
-    # For example:
-    prediction = model.predict([[float(input_text)]])
+    df = df[["gender", "race/ethnicity", "parental level of education", "lunch", "test preparation course", "math score", "reading score"]]
 
-    # Make sure to return the prediction result
-    return prediction[0][0]
+    # Call your model to make predictions on the dataframe
+    # For example:
+    predictions = model.predict(df)
+
+    # Predictions DF
+    dfPredictions = pd.DataFrame(predictions, columns=(["Writing score"]))
+
+    # Make sure to return the prediction results
+    return dfPredictions
 
 # Run the app
 if __name__ == "__main__":
